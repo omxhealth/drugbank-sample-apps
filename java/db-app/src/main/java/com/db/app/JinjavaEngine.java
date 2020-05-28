3441fca3c7d08ae505dcc01516c9041ea84e6d1d
Keegan Petreman <keeganp218@hotmail.com>
2020-05-28 16:55:37 -0600

    Reworked the file structure and lots of testing
    Took longer than expected, but the java app is working with the resources directory moved to outside the java app classpath (maven looks for it at {project}/src/main/resources and I moved it to {project}/../resources). Note that depending on where the app is run, the resources won't be found: this was the thing I spent most of my time trying to fix, but I may have to move on for now.

---
+++ java/db-app/src/main/java/com/db/app/JinjavaEngine.java
@@ -0,0 +1,93 @@
+package com.db.app;
+
+import com.hubspot.jinjava.*;
+
+import spark.ModelAndView;
+import spark.TemplateEngine;
+import spark.Spark.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.util.Map;
+
+/**
+ * Jinjava as Spark TemplateEngine.
+ * Taken from https://github.com/n-at/spark-with-jinjava
+ */	
+public class JinjavaEngine extends TemplateEngine {
+
+    String templatesPath = null;
+
+    Jinjava renderer = null;
+    JinjavaConfig rendererConfig = null;
+
+    TemplateLocator locator = null;
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    public JinjavaEngine() {
+        this("templates");
+    }
+
+    public JinjavaEngine(String templatesPath) {
+        this.templatesPath = Paths.get(templatesPath)
+                .toAbsolutePath().toString();
+        configure();
+    }
+
+    @Override
+    public String render(ModelAndView modelAndView) {
+        try {
+            String template = locator.getString(modelAndView.getViewName(),
+                    StandardCharsets.UTF_8, null);
+            return renderer.render(template, (Map<String, ?>)modelAndView.getModel());
+        } catch(IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    /**
+     * Setup renderer
+     */
+    protected void configure() {
+        if(rendererConfig == null) {
+            renderer = new Jinjava();
+            rendererConfig = renderer.getGlobalConfig();
+        } else {
+            renderer = new Jinjava(rendererConfig);
+        }
+
+        locator = new TemplateLocator(templatesPath);
+        renderer.setResourceLocator(locator);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Ser renderer config
+     * @param config JinjavaConfig
+     */
+    public void setConfig(JinjavaConfig config) {
+        rendererConfig = config;
+        renderer = new Jinjava(rendererConfig);
+        renderer.setResourceLocator(locator);
+    }
+
+    /**
+     * Get renderer configuration
+     * @return JinjavaConfig
+     */
+    public JinjavaConfig getConfig() {
+        return rendererConfig;
+    }
+
+    public boolean isUseCache() {
+        return locator.isUseCache();
+    }
+
+    public void setUseCache(boolean useCache) {
+        locator.setUseCache(useCache);
+    }
+
+}
