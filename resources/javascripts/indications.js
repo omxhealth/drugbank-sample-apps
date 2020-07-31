d3cb3a8a43b6b37c7f6a3701160e7e45bdff80bd
Keegan Petreman <keeganp218@hotmail.com>
2020-07-31 16:59:01 -0600

    Added the welcome page
    Other changes:
    - Redesigned the appearance of options in the DDI list
    - Added animations to the DDI drug list
    - Fixed an issue where the search terms on the result page weren't vertically centered in their parent
    - Fixed an issue where the text in the Select2 selects weren't centered
    - Changed :focus color from blue to purple
    - Scaled down the search button to 80%
    - Lots of documentation across the JavaScript files

--- resources/javascripts/indications.js
+++ resources/javascripts/indications.js
@@ -2,7 +2,13 @@
  * DrugBank API Sample App
  * indications.js
  * 
- * Functions for the indications search page.
+ * JavaScript that's run on the "Indications" section.
+ * 
+ * Most of the code here is just for functionality of the page.
+ * Parts involved in calling the API include: 
+ * 
+ *  - submitting the request on clicking the search button: 
+ *      $("button.search-button").on("click")
  */
 
 var api_key = getApiKey(); 
@@ -15,23 +21,12 @@ var indications_table = $('.indications-table').DataTable({
     ]
 });
 
-getUsage = function(indication) {
-    var usages;
-    usages = [];
-
-    if (indication.process) {
-      usages.push(indication.process.name);
-    }
-    if (indication.condition) {
-      usages.push(indication.condition.name);
-    }
-    if (indication.therapy) {
-      usages.push(indication.therapy.name);
-    }
-
-    return usages.join(", ");
-
-};
+/**
+ * Indications Page Functions
+ * 
+ * Includes functions for loading results, 
+ * and resetting the search.
+ */
 
 /**
  * Loads the results of the search into the table, then hides
@@ -79,7 +74,31 @@ loadTableResults = function(indications_table, data) {
 
     return indications_table.draw();
 
-  };
+};
+
+ /**
+ * Grabs the usages for a given hit from the API response.
+ * Grabs data from the JSON response and returns it on a 
+ * single line string for display in the table.
+ * Used in loadTableResults().
+ */
+getUsage = function(indication) {
+    var usages;
+    usages = [];
+
+    if (indication.process) {
+      usages.push(indication.process.name);
+    }
+    if (indication.condition) {
+      usages.push(indication.condition.name);
+    }
+    if (indication.therapy) {
+      usages.push(indication.therapy.name);
+    }
+
+    return usages.join(", ");
+
+};
 
 /**
  * Displays the selected search filters for specificity, 
@@ -140,6 +159,8 @@ searchReset = function() {
     $(".options-row").show();
 
     $(".search-button").removeClass("search-button-reset");
+    $(".search-button").addClass("search-button-disabled");
+    $(".search-button").attr("disabled", true);
     $(".search-button-text").html("Search");
 
     clearTableResults(indications_table);
@@ -148,122 +169,131 @@ searchReset = function() {
 
 }  
 
-$(document).ready(function() {
+/* End of functions */
 
-    $(".results-row").hide();
-    $(".search-button").attr("disabled", true);
+/**
+ * Initialization code for the page. 
+ * Initializes parts like nav underline, 
+ * and sets event listeners.
+ * 
+ * This could all go in $(document).ready(), but doing so 
+ * causes the nav underline to flicker on page load,
+ * and event listeners don't need to go in it.
+ */
 
-    $("#indications_nav").addClass("active");
-    navUnderlineSetup();
-    restyleDatatableFilter();
+$(".results-row").hide();
+$(".search-button").attr("disabled", true);
 
-    // Add blue underline when the search bar is in focus.
-    // Piggybacks off of the Selectize.js CSS.
-    $("#indication-name").on("focus", function(e) {
-        $(".selectize-input").addClass("focus");
-    });
-    
-    // Remove the blue underline when the search bar is out of focus.
-    // Piggybacks off of the Selectize.js CSS.
-    $("#indication-name").on("blur", function(e) {
-        $(".selectize-input").removeClass("focus");
-    });
+$("#indications_nav").addClass("active");
+navUnderlineSetup();
+restyleDatatableFilter();
 
-    // Grab all the kind options from the template and put into an array.
-    // These values are used for constructing the query sent to the API.
-    $(".kind-option").each(function() {
-        options[$(this).children("input").attr("value")] = $(this).children("label").text();
-    })
-
-    // Enable the search button once text is in the search box.
-    // Disable otherwise.
-    $("#indication-name").on("input", function(e) {
-
-        if ($("#indication-name").val()) {
-            $(".search-button").attr("disabled", false);
-            $(".search-button").removeClass("search-button-disabled");
-        } else {
-            $(".search-button").attr("disabled", true);
-            $(".search-button").addClass("search-button-disabled");
-        }
-    }); 
-        
-    // If the enter key is pressed when there is text in the 
-    // search box, act as if the search button has been pressed.
-    // Nothing happens if the search box is empty.
-    $(document).keypress(function(event) {
-        if (event.which == '13') {
-            event.preventDefault();
-            if ($("#indication-name").val() && $("#indication-name").prop("disabled") == false) {
-                $("button.search-button").trigger("click");
-            }
-        }
-    });
-    
-    // Search indications when form is submitted.
-    $("button.search-button").on("click", function(e) {
-
-        // If the results section is shown, the button is used for resetting the search
-        if ($(this).hasClass("search-button-reset")) {
-            searchReset();
-            return;
-        }
-        
-        $("#loader").show();
-        $("#indication-name").prop("disabled", true);
-        
-        var q = $("#indication-name").val();
-        var more = $("input[name='specificity']:checked").val();
-        var off_label = $("input[name='off_label']:checked").val();
-        var otc_use = $("input[name='otc_use']:checked").val();;
-        var indications_params = "?q=" + q;
-        var kind = [];
-
-        // Get all the checked therapy kinds and put them into array
-        $(".kind-option").each(function() { 
-            if ($(this).children("input").is(":checked")) {
-                kind.push($(this).children("input").attr("value"));
-            }
-        })
-        
-        if (more) {
-            indications_params += "&more=" + more;
-        }
-        if (kind) {
-            indications_params += "&kind=" + kind.join(",");
-        }
-        if (off_label) {
-            indications_params += "&off_label=" + off_label;
-        }
-        if (otc_use) {
-            indications_params += "&otc_use=" + otc_use;
-        }
-
-        return $.ajax({
-            url: localhost + encodeURI("indications" + indications_params),
-            // Brief delay to a) work around a select2 bug that is not patched in the version
-            // included in rails-select2 (https://github.com/select2/select2/issues/4205)
-            // and b) reduce the number of requests sent
-            delay: 100,
-            data: {
-                request_path: encodeURI(indications_params)
-            },
-            success: function(data) {
-
-                // Fill the side display
-                displayRequest(encodeURI(api_route + indications_params), data, api_key);
-                showSearchTerms();
-                // Load results in table and show the table
-                loadResults(data);
-                return $("#loader").hide();
-
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-                $("#loader").hide();
-                return handleError(jqXHR, "#indication-name");
-            }
-        });
-
-    });
-    
+// Add underline highlight when the search bar is in focus.
+// Piggybacks off of the Selectize.js CSS.
+$("#indication-name").on("focus", function(e) {
+    $(".selectize-input").addClass("focus");
 });
+
+// Remove the blue underline when the search bar is out of focus.
+// Piggybacks off of the Selectize.js CSS.
+$("#indication-name").on("blur", function(e) {
+    $(".selectize-input").removeClass("focus");
+});
+
+// Grab all the kind options from the template and put into an array.
+// These values are used for constructing the query sent to the API.
+$(".kind-option").each(function() {
+    options[$(this).children("input").attr("value")] = $(this).children("label").text();
+})
+
+// Enable the search button once text is in the search box.
+// Disable otherwise.
+$("#indication-name").on("input", function(e) {
+
+    if ($("#indication-name").val()) {
+        $(".search-button").attr("disabled", false);
+        $(".search-button").removeClass("search-button-disabled");
+    } else {
+        $(".search-button").attr("disabled", true);
+        $(".search-button").addClass("search-button-disabled");
+    }
+}); 
+    
+// If the enter key is pressed when there is text in the 
+// search box, act as if the search button has been pressed.
+// Nothing happens if the search box is empty.
+$(document).keypress(function(event) {
+    if (event.which == '13') {
+        event.preventDefault();
+        if ($("#indication-name").val() && $("#indication-name").prop("disabled") == false) {
+            $("button.search-button").trigger("click");
+        }
+    }
+});
+
+// Search indications when form is submitted.
+$("button.search-button").on("click", function(e) {
+
+    // If the results section is shown, the button is used for resetting the search
+    if ($(this).hasClass("search-button-reset")) {
+        searchReset();
+        return;
+    }
+    
+    $("#loader").show();
+    $("#indication-name").prop("disabled", true);
+    
+    var q = $("#indication-name").val();
+    var more = $("input[name='specificity']:checked").val();
+    var off_label = $("input[name='off_label']:checked").val();
+    var otc_use = $("input[name='otc_use']:checked").val();;
+    var indications_params = "?q=" + q;
+    var kind = [];
+
+    // Get all the checked therapy kinds and put them into array
+    $(".kind-option").each(function() { 
+        if ($(this).children("input").is(":checked")) {
+            kind.push($(this).children("input").attr("value"));
+        }
+    })
+    
+    if (more) {
+        indications_params += "&more=" + more;
+    }
+    if (kind) {
+        indications_params += "&kind=" + kind.join(",");
+    }
+    if (off_label) {
+        indications_params += "&off_label=" + off_label;
+    }
+    if (otc_use) {
+        indications_params += "&otc_use=" + otc_use;
+    }
+
+    return $.ajax({
+        url: localhost + encodeURI("indications" + indications_params),
+        // Brief delay to a) work around a select2 bug that is not patched in the version
+        // included in rails-select2 (https://github.com/select2/select2/issues/4205)
+        // and b) reduce the number of requests sent
+        delay: 100,
+        data: {
+            request_path: encodeURI(indications_params)
+        },
+        success: function(data) {
+
+            // Fill the side display
+            displayRequest(encodeURI(api_route + indications_params), data, api_key);
+            showSearchTerms();
+            // Load results in table and show the table
+            loadResults(data);
+            return $("#loader").hide();
+
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            $("#loader").hide();
+            return handleError(jqXHR, "#indication-name");
+        }
+    });
+
+});
+
---
+++ resources/javascripts/welcome.js
@@ -0,0 +1,114 @@
+/**
+ * DrugBank API Sample App:
+ * welcome.js
+ * 
+ * JavaScript that's run on the welcome page.
+ * 
+ * The code here is used for changing your API key and region, 
+ * which is done by communication with the local server.
+ * There is no code here that hits the DrugBank API.
+ */
+
+var api_key = getApiKey();
+var region = $("main")[0].attributes["region"].value;
+
+$(".db-logo").addClass("active");
+navUnderlineSetup();
+
+// Enable the tooltips
+$('.tooltip-container').tooltip({container:".col-settings"})
+
+// Get the API key and region and display them
+$("#auth_key_input").attr("placeholder", api_key);
+$("#region_select").val(region);
+
+/**
+ * Setting up the region select dropdown, which is a select2.
+ * This is outside the $(document).ready() as it flickers
+ * slightly as it changes from a regular select to a select2
+ */
+$("#region_select").select2({
+    theme: "drugbank",
+    minimumResultsForSearch: -1, // no search bar
+    width: "resolve" 
+});
+
+// On region change, send the newly selected region to the server
+$("#region_select").on("change", function() {
+    return $.ajax({
+        url: "/region",
+        type: "PUT",
+        contentType: "application/json",
+        data: JSON.stringify({
+            region: encodeURI($("#region_select").val())
+        }),
+        dataType: "json" 
+    });
+});
+
+// Prevent the enter key from submitting the form
+$("#auth_key_form").keypress(
+    function(event){
+        if (event.which == '13') {
+        event.preventDefault();
+        }
+    }
+);
+
+/**
+ * API key update button.
+ * On submission, send a JSON with the new key to the server.
+ * The server processes the request, and returns a message and whether 
+ * the key update was successful or not through http status code.
+ */
+$("#auth_key_submit").on("click", function(e) {
+    
+    e.preventDefault(); // Prevent form submission
+    e.stopPropagation();
+
+    return $.ajax({
+        url: "/auth_key",
+        type: "PUT",
+        contentType: "application/json",
+        data: JSON.stringify({
+            q: encodeURI($("#auth_key_input").val())
+        }),
+        dataType: "json",
+        success: function(result){
+            $("#auth_key_alert").fadeOut(100); // if an alert wasn't dismissed, hide it now
+            $("#auth_key_input").attr("placeholder", result["updated-key"]); // put new key
+            $("#auth_key_input").val(""); // empty the text input
+            $("#auth_update_message").text((result)["message"]); // set message
+            $("#auth_key_alert").removeClass("alert-warning"); // remove alert-warning class (makes yellow)
+            $("#auth_key_alert").addClass("alert-success"); // add alert-success class (makes blue)
+            $("#auth_key_alert").fadeIn(200); // bring in the alert
+        },
+
+        // An error is caused by either the new key being empty
+        // or by being unable to open "config.json" to save
+        error: function(xhr) {
+            var result = JSON.parse(xhr.responseText);
+            $("#auth_key_alert").fadeOut(100); // if an alert wasn't dismissed, hide it now
+            $("#auth_key_input").attr("placeholder", result["original-key"]); // put original key
+            $("#auth_key_input").val(""); // empty the text input
+            $("#auth_update_message").text((result)["message"]); // set message
+            $("#auth_key_alert").removeClass("alert-success"); // remove alert-success class (makes blue)
+            $("#auth_key_alert").addClass("alert-warning"); // add alert-warning class (makes yellow)
+            $("#auth_key_alert").fadeIn(200); // bring in the alert
+        }
+
+    });
+
+});
+
+/**
+ * Function for hiding alert pop up when "x" is clicked as
+ * opposed to the alert being completely removed from the DOM.
+ * This allows for reuse of the alert.
+ * Taken from https://stackoverflow.com/a/11029624/12471692.
+ */   
+$(function(){
+    $("[data-hide]").on("click", function(){
+        $(this).closest("." + $(this).attr("data-hide")).fadeOut(200);
+    });
+});
